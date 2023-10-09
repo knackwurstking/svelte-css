ee8073b52e24c989bdb2f8c69148362c216f37c7
Udo Bauer <knackwurstking.tux@gmail.com>
2023-10-09 20:18:46 +0200

    kick dialogs from misc/

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,6 @@
 export * from "./button";
 export * from "./text";
-export * from "./misc/dialogs";
 export * from "./misc/date-picker";
 export * from "./misc/shift";
 export * from "./css";
+export * as utils from "./utils";
--- src/lib/misc/dialogs/index.js
+++
@@ -1,5 +0,0 @@
-export { default as EditDayDialog } from "./EditDayDialog.svelte";
-export { default as ShiftDialog } from "./ShiftDialog.svelte";
-export { default as EditRhythmDialog } from "./EditRhythmDialog.svelte";
-export { default as StorageDialog } from "./StorageDialog.svelte";
-export { default as DatePickerDialog } from "./DatePickerDialog.svelte";
