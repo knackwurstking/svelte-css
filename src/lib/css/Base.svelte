4d13e38a8c5e7fb0feddf2ad2adc7401bfa38935
Udo Bauer <knackwurstking.tux@gmail.com>
2023-10-10 13:45:32 +0200

    update dark/light themes and base

--- src/lib/css/Base.svelte
+++ src/lib/css/Base.svelte
@@ -1,10 +1,40 @@
 <script>
+    import { onDestroy } from "svelte";
     import "./main.css";
 
     import { CSSThemeDark, CSSThemeLight } from "./themes";
 
     /** @type {"light" | "dark"} */
     export let theme = "light";
+    /** @type {boolean} */
+    export let auto = false;
+    $: auto && enableAutoThemeSwitcher();
+
+    /** @type {MediaQueryList} */
+    let media;
+
+    function enableAutoThemeSwitcher() {
+        if (auto) {
+            if (window.matchMedia) {
+                const media = window.matchMedia("(prefers-color-scheme: dark)");
+                theme = media.matches ? "dark" : "light";
+                media.addEventListener("change", onDarkChange);
+            }
+        }
+    }
+
+    /** @param {MediaQueryListEvent} ev */
+    const onDarkChange = (ev) => {
+        if (ev.matches) {
+            theme = "dark";
+        } else {
+            theme = "light";
+        }
+    };
+
+    onDestroy(() => {
+        if (media) media.removeEventListener("change", onDarkChange);
+    });
 </script>
 
 <svelte:head>
--- src/lib/css/themes/Dark.svelte
+++ src/lib/css/themes/Dark.svelte
@@ -1,3 +1,47 @@
 <script>
-    import "./dark.css";
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        const style = document.createElement("style");
+        style.innerText = `
+            :root {
+                --color-bg-color: hsl(205, 10%, 0%);
+                --color-bg-input: hsl(205, 0%, 5%);
+                --color-text: hsla(240, 10%, 90%, .9);
+
+                --color-primary: hsl(105, 50%, 50%);
+                --color-primary-hover: hsl(105, 65%, 60%);
+                --color-primary-hover-alpha: hsla(105, 50%, 50%, .25);
+                --color-primary-active: hsl(105, 75%, 70%);
+                --color-primary-active-alpha: hsla(105, 50%, 50%, .4);
+
+                --color-secondary: hsl(240, 50%, 50%);
+                --color-secondary-hover: hsl(240, 50%, 60%);
+                --color-secondary-hover-alpha: hsla(240, 50%, 50%, .25);
+                --color-secondary-active: hsl(240, 50%, 70%);
+                --color-secondary-active-alpha: hsla(240, 50%, 50%, .4);
+
+                --color-grey: hsl(0, 0%, 30%);
+                --color-grey-hover: hsl(0, 0%, 45%);
+                --color-grey-hover-alpha: hsla(0, 0%, 30%, .25);
+                --color-grey-active: hsl(0, 0%, 55%);
+                --color-grey-active-alpha: hsla(0, 0%, 30%, .6);
+
+                --color-primary-text: hsla(105, 50%, 50%, .85);
+                --color-secondary-text: hsla(240, 50%, 50%, .85);
+
+                --color-error: hsl(0, 100%, 50%);
+                --color-success: hsl(120, 100%, 50%);
+
+                --border-radius: 5px;
+                --border-width: 1px;
+                --border-color: var(--color-grey-active-alpha);
+
+                --spacing: 0.5rem;
+            }
+        `;
+        const toRemove = document.head.querySelector("style.theme");
+        if (toRemove) document.head.removeChild(toRemove);
+        document.head.append(style);
+    });
 </script>
