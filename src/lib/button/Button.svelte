da68d4750a08f3fd1a1627a0adca8f96cf5411e4
Udo Bauer <knackwurstking.tux@gmail.com>
2023-10-23 05:35:21 +0200

    Adding ripple

--- src/lib/button/Button.svelte
+++ src/lib/button/Button.svelte
@@ -5,13 +5,15 @@
     export let color = "primary";
     /** @type {"full" | "outline" | "ghost"} */
     export let variant = "full";
+    /** @type {import("../ripple").RippleOptions} */
     export let ripple = {};
     export let noRipple = false;
 </script>
 
 <button
     {...$$restProps}
-    class={`button ${color || ""} ${variant || ""}` + ($$restProps.class || "")}
+    class={`button ${color || ""} ${variant || ""} ` +
+        ($$restProps.class || "")}
     use:Ripple={noRipple ? null : ripple}
     on:click
 >
--- src/lib/button/IconButton.svelte
+++ src/lib/button/IconButton.svelte
@@ -1,11 +1,17 @@
 <script>
+    import { Ripple } from "../ripple";
+
     /** @type {"primary" | "secondary" | "destructive"} */
     export let color = "primary";
+    /** @type {import("../ripple").RippleOptions} */
+    export let ripple = {};
+    export let noRipple = false;
 </script>
 
 <button
     {...$$restProps}
     class={`icon-button ${color || ""} ` + ($$restProps.class || "")}
+    use:Ripple={noRipple ? null : ripple}
     on:click
 >
     <slot />
