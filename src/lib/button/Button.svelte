fd2411706447b7981041eb0f30e9270ba1d39b8f
Udo Bauer <knackwurstking.tux@gmail.com>
2023-10-23 04:08:46 +0200

    Add ripple to button

--- src/lib/button/Button.svelte
+++ src/lib/button/Button.svelte
@@ -1,13 +1,18 @@
 <script>
+    import { Ripple } from "../ripple";
+
     /** @type {"primary" | "secondary" | "destructive"} */
     export let color = "primary";
     /** @type {"full" | "outline" | "ghost"} */
     export let variant = "full";
+    export let ripple = {};
+    export let noRipple = false;
 </script>
 
 <button
     {...$$restProps}
     class={`button ${color || ""} ${variant || ""}` + ($$restProps.class || "")}
+    use:Ripple={noRipple ? null : ripple}
     on:click
 >
     <slot />
