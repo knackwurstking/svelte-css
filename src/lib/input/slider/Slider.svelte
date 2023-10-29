e577a297667c8dbcf6af714018e0716d2edd5e41
Udo Bauer <knackwurstking.tux@gmail.com>
2023-10-29 21:25:44 +0100

    @todo: input slider component

---
+++ src/lib/input/slider/Slider.svelte
@@ -0,0 +1,13 @@
+<script>
+    // TODO: simple slider component
+</script>
+
+<span class="slider">
+    <span class="range-container">
+        <span class="range" />
+    </span>
+    <span class="thumb" />
+</span>
+
+<style>
+</style>
