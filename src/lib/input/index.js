10c1fdee9c4535bc404a38a4986ca7a71b83aa7e
Udo Bauer <knackwurstking.tux@gmail.com>
2023-11-01 15:42:19 +0100

    Adding number input component with validation

--- src/lib/input/index.js
+++ src/lib/input/index.js
@@ -1,3 +1,4 @@
 export { default as Select } from "./select";
 export { default as Slider } from "./slider";
 export { default as Text } from "./text";
+export { default as Number } from "./number";
---
+++ src/lib/input/number/Number.svelte
@@ -0,0 +1,68 @@
+<script>
+    /** @type {string} */
+    export let title = "";
+
+    /** @type {string} */
+    export let placeholder = "";
+
+    /** @type {number} */
+    export let min = undefined;
+    /** @type {number} */
+    export let max = undefined;
+
+    /** @type {number} */
+    export let value = undefined;
+
+    export let invalid = false;
+
+    let focus = false;
+</script>
+
+<div {...$$restProps} class={"input " + $$restProps.class || ""} class:focus={focus} class:invalid={invalid}>
+    <span class="title">{title}</span>
+    <input
+        type="number"
+        {placeholder}
+        {min}
+        {max}
+        bind:value
+        on:change
+        on:focus={() => focus = true}
+        on:blur={() => focus = false}
+    />
+</div>
+
+<style>
+    .input {
+        width: 100%;
+        border-bottom: var(--border-width) solid hsl(var(--border));
+        transition: border-color .25s linear;
+    }
+
+    .input.focus {
+        border-color: hsl(var(--primary));
+    }
+
+    .input.invalid {
+        border-color: hsl(var(--destructive));
+    }
+
+    .input .title {
+        font-size: 0.85em;
+        font-weight: 300;
+        font-style: italic;
+        padding-left: var(--spacing);
+        padding-right: var(--spacing);
+    }
+
+    input {
+        width: 100%;
+
+        margin: 0;
+        padding-top: calc(var(--spacing) / 1.5);
+
+        border: none;
+        outline-offset: 0 !important;
+        outline: none !important;
+    }
+</style>
