3d6dc104b20fc3a4508b7837ed332574c173481a
Udo Bauer <knackwurstking.tux@gmail.com>
2023-11-06 15:29:12 +0100

    Export types

---
+++ src/lib/index.d.ts
@@ -0,0 +1,7 @@
+export * as Button from "./button";
+export * as CSS from "./css";
+export * as Dialog from "./dialog";
+export * as Input from "./input";
+export * as Ripple from "./ripple";
+export * as Text from "./text";
+export * as Utils from "./utils";
