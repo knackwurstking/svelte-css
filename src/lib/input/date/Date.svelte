59a37910bf8ea4812cbfa583e3ed7361e1ee8359
Udo Bauer <knackwurstking.tux@gmail.com>
2023-11-07 17:08:04 +0100

    Add date input component

---
+++ src/lib/input/date/Date.svelte
@@ -0,0 +1,79 @@
+<script>
+    /******************************
+     * Variable Export Definitions
+     ******************************/
+
+    export let title = "";
+    export let value = "";
+
+    /** @type {string} */
+    export let min = undefined;
+    /** @type {string} */
+    export let max = undefined;
+
+    export let invalid = false;
+
+    /***********************
+     * Variable Definitions
+     ***********************/
+
+    let focus = false;
+</script>
+
+<div
+    {...$$restProps}
+    class={"input " + $$restProps.class || ""}
+    class:focus={focus}
+    class:invalid={invalid}
+>
+    {#if !!title}
+        <span class="title">{title}</span>
+    {/if}
+    <input
+        type="date"
+        {min}
+        {max}
+        bind:value
+        on:change
+        on:input
+        on:focus={() => focus = true}
+        on:blur={() => focus = false}
+    />
+</div>
+
+<style>
+    .input {
+        width: fit-content;
+
+        border-bottom: var(--border-width) solid hsl(var(--border));
+
+        transition: border-color .25s linear;
+    }
+
+    .input.focus {
+        border-color: hsl(var(--primary));
+    }
+
+    .input.invalid {
+        border-color: hsl(var(--destructive));
+    }
+
+    .input .title {
+        font-size: 0.85em;
+        font-weight: 300;
+        font-style: italic;
+        padding-left: var(--spacing);
+        padding-right: var(--spacing);
+    }
+
+    input {
+        width: fit-content;
+
+        margin: 0;
+        padding-top: calc(var(--spacing) / 1.5);
+
+        border: none;
+        outline-offset: 0 !important;
+        outline: none !important;
+    }
+</style>
