210d59fcae93bb0dd82c0801ca097a04d02dcc52
Udo Bauer <knackwurstking.tux@gmail.com>
2023-11-07 17:09:53 +0100

    Adding exports (input month/date)

--- src/lib/input/index.js
+++ src/lib/input/index.js
@@ -1,4 +1,6 @@
 export { default as Select } from "./select";
 export { default as Slider } from "./slider";
-export { default as Text } from "./text";
+export { default as Text   } from "./text";
 export { default as Number } from "./number";
+export { default as Month  } from "./month";
+export { default as Date   } from "./date";
