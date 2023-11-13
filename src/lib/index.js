889aae45af807fd8a7fb10fd6211c865337e01e7
Udo Bauer <knackwurstking.tux@gmail.com>
2023-11-13 15:24:25 +0100

    remove list for now

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,7 +2,6 @@ export * as Button from "./button";
 export * as CSS from "./css";
 export * as Dialog from "./dialog";
 export * as Input from "./input";
-export * as List from "./list";
 export * as Ripple from "./ripple";
 export * as Text from "./text";
 export * as Utils from "./utils";
