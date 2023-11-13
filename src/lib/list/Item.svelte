e77634ade3fd2fdce8e0832ee3cc88764268cfd4
Udo Bauer <knackwurstking.tux@gmail.com>
2023-11-13 15:02:47 +0100

    Adding list + item

--- src/lib/list/Item.svelte
+++ src/lib/list/Item.svelte
@@ -3,24 +3,35 @@
 
     export let primary = "";
     export let secondary = "";
+    export let useRipple = false;
+    export let checked = false;
 </script>
 
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
 <div
     class="item no-user-select"
-    use:Ripple.Root
+    class:checked
+    use:Ripple.Root={useRipple ? undefined : null}
+    on:click
 >
-    <Text.Label
-        {primary}
-        {secondary}
-        useLabel
-    >
-       <input type="checkbox"  style="display: none" />
-    </Text.Label>
+    <div class="background" />
+    <Text.Label {primary} {secondary} />
 </div>
 
 <style>
     .item {
         padding: var(--spacing);
         overflow: hidden;
+    }
+
+    .item.checked .background {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: currentColor;
+        opacity: .2;
     }
 </style>
--- src/lib/list/index.js
+++ src/lib/list/index.js
@@ -1,2 +1,11 @@
 export { default as Item } from "./Item.svelte";
 export { default as Root } from "./List.svelte";
+
+/**
+ * @typedef ListItem
+ * @type {{
+ *  primary?: string,
+ *  secondary?: string,
+ *  value: any,
+ * }}
+ */
