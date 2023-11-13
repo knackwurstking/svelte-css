00a0d541831cf77494f3d69282691d4166dd156b
Udo Bauer <knackwurstking.tux@gmail.com>
2023-11-13 16:32:37 +0100

    add spacer component

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,5 +3,6 @@ export * as CSS from "./css";
 export * as Dialog from "./dialog";
 export * as Input from "./input";
 export * as Ripple from "./ripple";
+export * as Spacer from "./spacer";
 export * as Text from "./text";
 export * as Utils from "./utils";
---
+++ src/lib/spacer/Spacer.svelte
@@ -0,0 +1,20 @@
+<script>
+    /** @type {"vertical" | "horizontal"} */
+    export let align;
+</script>
+
+{#if align === "vertical"}
+    <span
+        style={
+            "margin-left: calc(var(--spacing) / 2);" +
+            "margin-right: calc(var(--spacing) / 2);"
+        }
+    />
+{:else if align === "horizontal"}
+    <div
+        style={
+            "margin-top: calc(var(--spacing) / 2);" +
+            "margin-bottom: calc(var(--spacing) / 2);"
+        }
+    />
+{/if}
