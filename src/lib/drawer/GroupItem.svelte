24e410e7bd254322428b312f7b014f002d1f160a
Udo Bauer <knackwurstking.tux@gmail.com>
2023-11-14 13:55:44 +0100

    add button to drawer GroupItem

--- src/lib/drawer/GroupItem.svelte
+++ src/lib/drawer/GroupItem.svelte
@@ -1,7 +1,20 @@
+<script>
+    import { Button } from "..";
+
+    // TODO: add active prop
+    // TODO: set custom button color var(--fg)
+</script>
+
 <li
     {...$$restProps}
     class={"no-border " + ($$restProps.class || "")}
     style={"display: block;" + ($$restProps.stykle || "")}
 >
-    <slot style="width: 100%; height: 100%;" />
+    <Button.Root
+        style="width: 100%; height: 100%;"
+        variant="ghost"
+        on:click
+    >
+        <slot style="width: 100%; height: 100%;" />
+    </Button.Root>
 </li>
