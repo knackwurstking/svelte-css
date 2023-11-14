42d88c62fa529e54ade0cab06e3cef69f66c9727
Udo Bauer <knackwurstking.tux@gmail.com>
2023-11-14 14:12:33 +0100

    update index .ts file

--- src/lib/index.d.ts
+++ src/lib/index.d.ts
@@ -1,7 +1,10 @@
 export * as Button from "./button";
 export * as CSS from "./css";
 export * as Dialog from "./dialog";
+export * as Drawer from "./drawer";
 export * as Input from "./input";
 export * as Ripple from "./ripple";
+export * as Spacer from "./spacer";
 export * as Text from "./text";
+export * as TopAppBar from "./top-app-bar";
 export * as Utils from "./utils";
