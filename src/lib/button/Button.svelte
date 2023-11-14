ed5ff03f918dd12fbfed52f4c89f4325a6ff8c65
Udo Bauer <knackwurstking.tux@gmail.com>
2023-11-14 14:32:24 +0100

    add active prop

--- src/lib/button/Button.svelte
+++ src/lib/button/Button.svelte
@@ -11,15 +11,19 @@
     export let ripple = {};
 
     export let noRipple = false;
+
+    export let active = false;
 </script>
 
 <button
     {...$$restProps}
     class={`button ${color || ""} ${variant || ""} ` +
         ($$restProps.class || "")}
+    class:active
     use:Ripple.Root={noRipple ? null : ripple}
     on:click
 >
+    <div class="background"></div>
     <slot />
 </button>
 
@@ -35,7 +39,24 @@
         transition: background-color .2s ease;
     }
 
+    .button .background {
+        background-color: currentColor;
+        transition: background-color .25s ease;
+        opacity: 0;
+
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+
     /* variant: full */
+
+    .button.full.active .background {
+        transition: opacity .25s ease .4s;
+        opacity: .2;
+    }
 
     .button.full.primary:not(.outline, .none) {
         background-color: hsl(var(--primary));
@@ -57,6 +78,11 @@
 
     /* variant: outline */
 
+    .button.outline.active .background {
+        transition: opacity .25s ease .4s;
+        opacity: .2;
+    }
+
     .button.outline {
         border-color: currentColor;
         background-color: transparent;
@@ -75,6 +101,11 @@
     }
 
     /* variant: ghost */
+
+    .button.ghost.active .background {
+        transition: opacity .25s ease .4s;
+        opacity: .2;
+    }
 
     .button.ghost {
         border-color: transparent;
