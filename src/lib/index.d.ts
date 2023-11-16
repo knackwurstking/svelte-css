7c26cfc155b86a5ab63d6e28fbe01855c4e80b26
Udo Bauer <knackwurstking.tux@gmail.com>
2023-11-16 17:31:41 +0100

    spacer exports removed from index

--- src/lib/index.d.ts
+++ src/lib/index.d.ts
@@ -6,7 +6,6 @@ export * as Drawer from "./drawer";
 export * as Grid from "./grid";
 export * as Input from "./input";
 export * as Ripple from "./ripple";
-export * as Spacer from "./spacer";
 export * as Text from "./text";
 export * as TopAppBar from "./top-app-bar";
 export * as Utils from "./utils";
