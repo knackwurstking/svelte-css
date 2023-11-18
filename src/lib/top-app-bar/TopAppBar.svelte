cb749782f4bcd98ad03eaac5abf73f70e9bbdc42
Udo Bauer <knackwurstking.tux@gmail.com>
2023-11-18 23:19:55 +0100

    replace flex div with flex-grid row container

--- src/lib/top-app-bar/TopAppBar.svelte
+++ src/lib/top-app-bar/TopAppBar.svelte
@@ -10,10 +10,12 @@
     class={"ui-top-app-bar flex " + ($$restProps.class || "")}
     style:height={height}
 >
-    <div
-        class="flex nowrap align-center is-max-height"
-        class:ui-container={uiContainer}
-        class:ui-container-full={!uiContainer}
+    <FlexGrid.Row
+        class={
+            "no-padding is-max-height align-center " +
+            (uiContainer ? "ui-container ": "")
+        }
+        gap=".1em"
     >
         <FlexGrid.Row class="justify-start" style="width: fit-content;" gap=".1em">
             <slot name="left" />
@@ -26,7 +28,7 @@
         <FlexGrid.Row class="justify-end" gap=".1em">
             <slot name="right" />
         </FlexGrid.Row>
-    </div>
+    </FlexGrid.Row>
 </header>
 
 <style>
