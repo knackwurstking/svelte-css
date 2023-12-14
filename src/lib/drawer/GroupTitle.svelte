af91f0d78e4199ce96e0845f63f9e993157e62de
Udo Bauer <knackwurstking.tux@gmail.com>
2023-12-14 02:15:52 +0100

    add GroupItem container to GroupTitle component

--- src/lib/drawer/GroupTitle.svelte
+++ src/lib/drawer/GroupTitle.svelte
@@ -1,6 +1,12 @@
-<h2
-    {...$$restProps}
-    class={"ui-drawer-group-title " + ($$restProps.class || "")}
->
-    <slot />
-</h2>
+<script>
+    import { GroupItem } from "."
+</script>
+
+<GroupItem>
+    <h2
+        {...$$restProps}
+        class={"ui-drawer-group-title " + ($$restProps.class || "")}
+    >
+        <slot />
+    </h2>
+</GroupItem>
